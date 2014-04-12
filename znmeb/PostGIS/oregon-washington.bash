d7dbba70f06528281138590501933c36adbba17c
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-04-11 21:41:06 -0700

    scripts to download TIGER shapefiles US states, counties, WA and OR detail

---
+++ znmeb/PostGIS/oregon-washington.bash
@@ -0,0 +1,21 @@
+#! /bin/bash -v
+wget ftp://ftp2.census.gov/geo/tiger/TIGER2013/PLACE/tl_*_41_* --no-parent --relative --recursive --level=2 --accept=zip --mirror --reject=html 
+wget ftp://ftp2.census.gov/geo/tiger/TIGER2013/COUSUB/tl_*_41_* --no-parent --relative --recursive --level=2 --accept=zip --mirror --reject=html 
+wget ftp://ftp2.census.gov/geo/tiger/TIGER2013/TRACT/tl_*_41_* --no-parent --relative --recursive --level=2 --accept=zip --mirror --reject=html 
+wget ftp://ftp2.census.gov/geo/tiger/TIGER2013/TABBLOCK/tl_*_41_* --no-parent --relative --recursive --level=2 --accept=zip --mirror --reject=html 
+wget ftp://ftp2.census.gov/geo/tiger/TIGER2013/BG/tl_*_41_* --no-parent --relative --recursive --level=2 --accept=zip --mirror --reject=html 
+wget ftp://ftp2.census.gov/geo/tiger/TIGER2010/ZCTA5/2010/*_41* --no-parent --relative --recursive --level=2 --accept=zip --mirror --reject=html 
+wget ftp://ftp2.census.gov/geo/tiger/TIGER2013/FACES/*_41* --no-parent --relative --recursive --level=2 --accept=zip --mirror --reject=html 
+wget ftp://ftp2.census.gov/geo/tiger/TIGER2013/FEATNAMES/*_41* --no-parent --relative --recursive --level=2 --accept=zip --mirror --reject=html 
+wget ftp://ftp2.census.gov/geo/tiger/TIGER2013/EDGES/*_41* --no-parent --relative --recursive --level=2 --accept=zip --mirror --reject=html 
+wget ftp://ftp2.census.gov/geo/tiger/TIGER2013/ADDR/*_41* --no-parent --relative --recursive --level=2 --accept=zip --mirror --reject=html 
+wget ftp://ftp2.census.gov/geo/tiger/TIGER2013/PLACE/tl_*_53_* --no-parent --relative --recursive --level=2 --accept=zip --mirror --reject=html 
+wget ftp://ftp2.census.gov/geo/tiger/TIGER2013/COUSUB/tl_*_53_* --no-parent --relative --recursive --level=2 --accept=zip --mirror --reject=html 
+wget ftp://ftp2.census.gov/geo/tiger/TIGER2013/TRACT/tl_*_53_* --no-parent --relative --recursive --level=2 --accept=zip --mirror --reject=html 
+wget ftp://ftp2.census.gov/geo/tiger/TIGER2013/TABBLOCK/tl_*_53_* --no-parent --relative --recursive --level=2 --accept=zip --mirror --reject=html 
+wget ftp://ftp2.census.gov/geo/tiger/TIGER2013/BG/tl_*_53_* --no-parent --relative --recursive --level=2 --accept=zip --mirror --reject=html 
+wget ftp://ftp2.census.gov/geo/tiger/TIGER2010/ZCTA5/2010/*_53* --no-parent --relative --recursive --level=2 --accept=zip --mirror --reject=html 
+wget ftp://ftp2.census.gov/geo/tiger/TIGER2013/FACES/*_53* --no-parent --relative --recursive --level=2 --accept=zip --mirror --reject=html 
+wget ftp://ftp2.census.gov/geo/tiger/TIGER2013/FEATNAMES/*_53* --no-parent --relative --recursive --level=2 --accept=zip --mirror --reject=html 
+wget ftp://ftp2.census.gov/geo/tiger/TIGER2013/EDGES/*_53* --no-parent --relative --recursive --level=2 --accept=zip --mirror --reject=html 
+wget ftp://ftp2.census.gov/geo/tiger/TIGER2013/ADDR/*_53* --no-parent --relative --recursive --level=2 --accept=zip --mirror --reject=html 
