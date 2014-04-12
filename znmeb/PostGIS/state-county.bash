9356dad7bb64f1eb69fd2e8fb9e74af0fe25d77d
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-04-12 10:03:20 -0700

    no longer needed

--- znmeb/PostGIS/state-county.bash
+++
@@ -1,3 +0,0 @@
-#! /bin/bash -v
-wget ftp://ftp2.census.gov/geo/tiger/TIGER2013/STATE/ --no-parent --relative --recursive --level=1 --accept=zip --mirror --reject=html 
-wget ftp://ftp2.census.gov/geo/tiger/TIGER2013/COUNTY/ --no-parent --relative --recursive --level=1 --accept=zip --mirror --reject=html 
