f2e24ace507af9ae0e9717dbb209efa9080cd118
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-04-13 12:19:37 -0700

    switched to 'geocoder' database

--- znmeb/PostGIS/make-tiger-scripts.bash
+++ znmeb/PostGIS/make-tiger-scripts.bash
@@ -1,16 +1,25 @@
 #! /bin/bash -v
 
 # make workspace
-sudo rm -fr /gisdata
+sudo rm -fr /gisdata/temp
 sudo mkdir -p /gisdata/temp
 sudo chown -R ${USER}:${USER} /gisdata
 
 # execute script builder
-psql -f make-tiger-scripts.psql ballotpath znmeb
+psql -f make-tiger-scripts.psql geocoder znmeb
 chmod +x /gisdata/*.bash
-sed -i \
-  's;export PGBIN=/usr/pgsql-9.0/bin;export PGBIN=/usr/bin;' \
-  /gisdata/state-county.bash
-sed -i \
-  's;export PGBIN=/usr/pgsql-9.0/bin;export PGBIN=/usr/bin;' \
-  /gisdata/oregon-washington.bash
+
+# fix path
+for i in '/gisdata/state-county.bash' '/gisdata/oregon-washington.bash'
+do
+  sed -i 's;export PGBIN=/usr/pgsql-9.0/bin;export PGBIN=/usr/bin;' ${i}
+done
+
+# comment out 'localhost' setting so we can use UNIX sockets
+for i in '/gisdata/state-county.bash' '/gisdata/oregon-washington.bash'
+do
+  sed -i 's;export PGHOST=localhost;#export PGHOST=localhost;' ${i}
+done
+
+# change owner to 'postgres'
+sudo chown -R postgres:postgres /gisdata
