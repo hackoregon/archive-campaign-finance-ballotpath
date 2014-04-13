07f279454e901214370483c01a3e6b515d0b6c82
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-04-12 17:38:44 -0700

    cleanup - just PostGIS, QGIS and OpenStreetMap

--- znmeb/PostGIS/yum-install-dependencies.bash
+++ znmeb/PostGIS/yum-install-dependencies.bash
@@ -1,33 +1,19 @@
 sudo cp qgis.repo /etc/yum.repos.d
 sudo yum update
 sudo yum install -y \
+  postgis \
   postgis-docs \
   postgis-utils \
   pgRouting \
-  postgis \
   pgadmin3 \
   qgis \
   qgis-grass \
   qgis-mapserver \
   qgis-python \
-  dans-gdal-scripts \
-  gdal \
-  gdal-doc \
-  gdal-perl \
-  gdal-python \
-  gdal-ruby \
-  gtatool-gdal \
-  pfstools-gdal \
-  geos \
-  geos-python \
   josm \
-  dosm \
   merkaartor \
   osm-gps-map \
   osm2pgsql \
   osm-gps-map \
   readosm \
-  routino \
-  proj \
-  proj-epsg \
-  proj-nad
+  routino
