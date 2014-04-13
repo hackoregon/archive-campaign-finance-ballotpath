b344d536c606a51867f180dd31f3552c6ca2d303
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-04-12 19:23:07 -0700

    factored sudo to calling script

--- znmeb/PostGIS/create-tiger-schema.bash
+++ znmeb/PostGIS/create-tiger-schema.bash
@@ -1,9 +1,19 @@
-﻿CREATE EXTENSION postgis;
-CREATE EXTENSION fuzzystrmatch;
-CREATE EXTENSION postgis_tiger_geocoder;
-GRANT USAGE ON SCHEMA tiger TO PUBLIC;
-GRANT USAGE ON SCHEMA tiger_data TO PUBLIC;
-GRANT SELECT, REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA tiger TO PUBLIC;
-GRANT SELECT, REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA tiger_data TO PUBLIC;
-GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA tiger TO PUBLIC;
-ALTER DEFAULT PRIVILEGES IN SCHEMA tiger_data GRANT SELECT, REFERENCES ON TABLES TO PUBLIC;
+#! /bin/bash
+su - postgres -c \
+  "psql -d ${1} -c 'CREATE EXTENSION postgis;'"
+su - postgres -c \
+  "psql -d ${1} -c 'CREATE EXTENSION fuzzystrmatch;'"
+su - postgres -c \
+  "psql -d ${1} -c 'CREATE EXTENSION postgis_tiger_geocoder;'"
+su - postgres -c \
+  "psql -d ${1} -c 'GRANT USAGE ON SCHEMA tiger TO PUBLIC;'"
+su - postgres -c \
+  "psql -d ${1} -c 'GRANT USAGE ON SCHEMA tiger_data TO PUBLIC;'"
+su - postgres -c \
+  "psql -d ${1} -c 'GRANT SELECT, REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA tiger TO PUBLIC;'"
+su - postgres -c \
+  "psql -d ${1} -c 'GRANT SELECT, REFERENCES, TRIGGER ON ALL TABLES IN SCHEMA tiger_data TO PUBLIC;'"
+su - postgres -c \
+  "psql -d ${1} -c 'GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA tiger TO PUBLIC;'"
+su - postgres -c \
+  "psql -d ${1} -c 'ALTER DEFAULT PRIVILEGES IN SCHEMA tiger_data GRANT SELECT, REFERENCES ON TABLES TO PUBLIC;'"
