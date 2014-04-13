3810583e6ce56e0bb641b4496d198b528ee19fdf
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-04-12 17:40:47 -0700

    cleanup

--- znmeb/PostGIS/yum-install-dependencies.bash
+++ znmeb/PostGIS/yum-install-dependencies.bash
@@ -1,5 +1,16 @@
+#! /bin/bash
+#
+# Copyright (C) 2013 by M. Edward (Ed) Borasky
+#
+# This program is licensed to you under the terms of version 3 of the
+# GNU Affero General Public License. This program is distributed WITHOUT
+# ANY EXPRESS OR IMPLIED WARRANTY, INCLUDING THOSE OF NON-INFRINGEMENT,
+# MERCHANTABILITY OR FITNESS FOR A PARTICULAR PURPOSE. Please refer to the
+# AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
+#
+
 sudo cp qgis.repo /etc/yum.repos.d
-sudo yum update
+sudo yum update -y
 sudo yum install -y \
   postgis \
   postgis-docs \
