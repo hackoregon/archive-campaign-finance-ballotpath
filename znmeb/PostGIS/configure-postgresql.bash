8b953905bdf750c3029d1578204767789f53d676
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-04-13 19:40:09 -0700

    refactored into one-time and regular reset scripts

--- znmeb/PostGIS/configure-postgresql.bash
+++ znmeb/PostGIS/configure-postgresql.bash
@@ -9,8 +9,6 @@
 # AGPL (http://www.gnu.org/licenses/agpl-3.0.txt) for more details.
 #
 
-export HERE=`pwd` # need absolute path later
-
 sudo postgresql-setup initdb # fails harmlessly if data directory isn't empty
 sudo systemctl enable postgresql # start the server on reboot
 sudo systemctl start postgresql # start the server now
@@ -21,45 +19,3 @@ sudo su - postgres -c "psql -c '\password postgres'"
 
 # install the extensions - will ERROR harmlessly if they're already there
 sudo su - postgres -c "psql -c 'CREATE EXTENSION adminpack;'"
-
-# PostgreSQL username = Linux username
-export PGUSER=${USER}
-
-# drop the old databases if any
-for i in \
-  ${PGUSER} \
-  geocoder \
-  congress_districts \
-  elementary_school_districts \
-  secondary_school_districts \
-  unified_school_districts \
-  state_legislature_lower_districts \
-  state_legislature_upper_districts
-do
-  sudo su - postgres -c "dropdb ${i}"
-done
-
-# drop and re-create the user
-sudo su - postgres -c "dropuser ${PGUSER}"
-sudo su - postgres -c "createuser -d ${PGUSER}"
-
-# create databases for the user
-for i in \
-  ${PGUSER} \
-  geocoder \
-  congress_districts \
-  elementary_school_districts \
-  secondary_school_districts \
-  unified_school_districts \
-  state_legislature_lower_districts \
-  state_legislature_upper_districts
-do
-  sudo su - postgres -c "createdb -O ${PGUSER} ${i}"
-  sudo ${HERE}/create-postgis.bash ${i}
-done
-
-# create the TIGER extensions in 'geocoder'
-sudo ${HERE}/create-tiger-schema.bash geocoder
-
-echo "Create a password for the PostgreSQL user '${PGUSER}'"
-psql -c '\password'
