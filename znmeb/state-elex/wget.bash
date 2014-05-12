36026a64fb1d7cff92daae610590867bd774e9bf
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-05-11 18:25:02 -0700

    script to mirror Oregon state election results

---
+++ znmeb/state-elex/wget.bash
@@ -0,0 +1,18 @@
+#! /bin/bash -v
+export GRAB='wget -m -k -K -E --user-agent="Mozilla/4.0" --accept=doc,pdf,htm,html,DOC,PDF,HTM,HTML'
+$GRAB http://www.oregonvotes.gov/pages/history/archive/nov62012/index.html
+$GRAB http://www.oregonvotes.gov/pages/history/archive/may152012/p2012results.html
+$GRAB http://www.oregonvotes.gov/pages/history/archive/jan312012/index.html
+$GRAB http://www.oregonvotes.gov/pages/history/archive/nov82011/index.html
+$GRAB http://www.oregonvotes.gov/pages/history/archive/nov022010/g2010results.html
+$GRAB http://www.oregonvotes.gov/pages/history/archive/may182010/p2010results.html
+$GRAB http://www.oregonvotes.gov/pages/history/archive/nov42008/g08results.html
+$GRAB http://www.oregonvotes.gov/pages/history/archive/may202008/p08results.html
+$GRAB http://www.oregonvotes.gov/pages/history/archive/nov72006/g06results.html
+$GRAB http://www.oregonvotes.gov/pages/history/archive/may162006/p06results.html
+$GRAB http://www.oregonvotes.gov/pages/history/archive/nov22004/g04abstract.html
+$GRAB http://www.oregonvotes.gov/pages/history/archive/may182004/p04abstract.html
+$GRAB http://www.oregonvotes.gov/pages/history/archive/nov52002/g02abstract.htm
+$GRAB http://www.oregonvotes.gov/pages/history/archive/may212002/abstracts/p02results.htm
+$GRAB http://www.oregonvotes.gov/pages/history/archive/nov72000/other.info/result.htm
+$GRAB http://www.oregonvotes.gov/pages/history/archive/may162000/results.html
