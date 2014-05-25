97c3caa82fb9ded24a48b92ed42c4c85b6aec000
M. Edward (Ed) Borasky <znmeb@znmeb.net>
2014-05-25 12:11:20 -0700

    just Oregon

--- znmeb/PostGIS/5make-geocoder-download-scripts.bash
+++ znmeb/PostGIS/5make-geocoder-download-scripts.bash
@@ -19,13 +19,13 @@ psql -f make-tiger-scripts.psql geocoder znmeb
 chmod +x /gisdata/*.bash
 
 # fix path
-for i in '/gisdata/state-county.bash' '/gisdata/oregon-washington.bash'
+for i in '/gisdata/state-county.bash' '/gisdata/oregon.bash'
 do
   sed -i 's;export PGBIN=/usr/pgsql-9.0/bin;export PGBIN=/usr/bin;' ${i}
 done
 
 # comment out 'localhost' setting so we can use UNIX sockets
-for i in '/gisdata/state-county.bash' '/gisdata/oregon-washington.bash'
+for i in '/gisdata/state-county.bash' '/gisdata/oregon.bash'
 do
   sed -i 's;export PGHOST=localhost;#export PGHOST=localhost;' ${i}
 done
